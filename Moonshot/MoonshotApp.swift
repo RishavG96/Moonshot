839cdbabfec0df026c197601cb74c44428620476
RishavG96 <gupta.rishav2608@gmail.com>
2023-06-07 07:37:32 +0530

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Rishav Gupta on 07/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
